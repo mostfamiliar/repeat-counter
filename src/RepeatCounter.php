e593ab53449805a1ddf09aec4cf5c8e04afcbc26
Torrence Stratton <tlstratton@gmail.com>
2016-02-19 08:27:19 -0800

    add one passing spec

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,5 +1,22 @@
 <?php
-class  {
+class  RepeatCounter {
+    function countRepeats($string_input, $word_input){
+        $string_array = explode(" ", $string_input);
+        $count = 0;
 
+        var_dump($string_array);
 
-} ?>
+        foreach($string_array as $string) {
+            echo $string;
+            if ($word_input == $string) {
+                $count++;
+            }
+        }
+
+        return $count;
+
+    }
+
+}
+
+?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -2,6 +2,47 @@
 require_once "src/RepeatCounter.php";
 
 class  RepeatCounterTest  extends PHPUnit_Framework_TestCase {
+    function test_countRepeats_oneWord() {
+
+        //arrange
+        $new_countRepeats = new RepeatCounter;
+        $string_input = "rain";
+        $word_input = "rain";
+
+        //act
+        $result = $new_countRepeats->countRepeats($string_input, $word_input);
+
+        //assert
+        $this->assertEquals(1, $result);
+    }
+    // 
+    // function test_countRepeats_sentence() {
+    //
+    //     //arrange
+    //     $new_countRepeats = new RepeatCounter;
+    //     $string_input = "I want to go to the store after school today";
+    //     $word_input = "to";
+    //
+    //     //act
+    //     $result = $new_countRepeats->countRepeats($string_input, $word_input);
+    //
+    //     //assert
+    //     $this->assertEquals(2, $result);
+    // }
+    //
+    // function test_countRepeats_partials() {
+    //
+    //     //arrange
+    //     $new_countRepeats = new RepeatCounter;
+    //     $string_input = "I listened to that record yesterday and then I brought it to the party. Do you have that record? It's a really good recording.";
+    //     $word_input = "record";
+    //
+    //     //act
+    //     $result = $new_countRepeats->countRepeats($string_input, $word_input);
+    //
+    //     //assert
+    //     $this->assertEquals(2, $result);
+    // }
 
 
 }
