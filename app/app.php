bd657ca6945468e9cbf2fa1fd0c45b272f27d4a7
Torrence Stratton <tlstratton@gmail.com>
2016-02-19 09:03:32 -0800

    add beginning UI

--- app/app.php
+++ app/app.php
@@ -9,7 +9,10 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
 
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
 
+    });
 
     return $app;
  ?>
